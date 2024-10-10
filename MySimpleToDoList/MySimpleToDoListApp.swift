27d0a10b6f9fa16c7606322cc217798b9912ea87
cfkim <christine.fth.kim@gmail.com>
2024-10-10 15:07:04 -0400

    Initial Commit

---
+++ MySimpleToDoList/MySimpleToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySimpleToDoListApp.swift
+//  MySimpleToDoList
+//
+//  Created by Christine Kim on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MySimpleToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
